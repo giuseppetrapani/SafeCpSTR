809aa74513bc19cb4093b5dce9c011f9a6088dff
lilkaneki <127780882+giuseppetrapani@users.noreply.github.com>
2025-09-19 16:36:29 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+//count: maximum number of characters to copy from src
+char *safeCpSTR(char *str, size_t count) {
+    char *dest = NULL;
+
+    if (str == NULL) return NULL;
+
+    dest = malloc(count+1); //+1 because \0
+
+    if (dest == NULL) {
+        printf("dest is null");
+        return NULL;
+    }
+    
+    for (size_t i = 0; i < count && str[i] != '\0'; i++) {
+        dest[i] = str[i];
+    }
+
+    dest[i] = '\0';
+   
+    return dest; 
+}
+
+int main(void) {
+    const char *str = "Hello";
+    size_t count = strlen(str); 
+
+    char *dest = safeCpSTR(str, count);
+
+    printf("STR: %s | DEST: %s\n", str, dest);
+
+    free(dest);
+
+    return 0;
+}
